9eb04a9f672d31eceeddf51cdb86f57a72782b9f
LORDAME <unlimit8.com@gmail.com>
2022-03-12 14:45:25 +0100

    adding exit export pwd unset

---
+++ mandatory/export.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orahmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 15:45:05 by orahmoun          #+#    #+#             */
+/*   Updated: 2022/03/12 11:15:20 by orahmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+bool  is_valid_key(char *key)
+{
+  size_t  i;
+
+  i = 1;
+  panic(key == NULL, "key is NULL", __func__);
+  if (key[0] == '\0'
+      || (ft_isalpha(key[0]) == 0
+      && key[0] != '_'))
+    return (false);
+  while (key[i])
+  {
+    if (ft_isalnum(key[i]) == 0 && key[i] != '_')
+      return (false);
+    i++;
+  }
+  return (true);
+}
+
+void  add_key(t_env **env, char *key, char *value)
+{
+  t_env *tmp;
+
+  tmp = find_key(*env, key);
+  if (tmp)
+  {
+    free(tmp->value);
+    tmp->value = ft_strdup(value);
+  }
+  else
+    add_variable_back(env,
+      create_variable(ft_strdup(key), ft_strdup(value)));
+}
+
+int bexport(char **args, t_env **env, int fd)
+{
+  size_t  i;
+  size_t  ret;
+  char    *tmp;
+
+  i = 0;
+  ret = 0;
+  if (*args == NULL)
+  {
+	  printf ("I'm here\n");
+	  benv(*env, fd);
+  }
+  while (args[i])
+  {
+    if (ft_strchr(args[i], '=') == NULL)
+    {
+      if (is_valid_key(args[i]) == false)
+        ret = printf ("export: not an identifier: %s\n", args[i]);
+    }
+    else
+    {
+      tmp = ft_strchr(args[i], '=');
+      tmp[0] = '\0';
+      if (is_valid_key(args[i]))
+        add_key(env, args[i], tmp + 1);
+      else
+        ret = printf ("export: not an identifier: %s\n", args[i]);
+      tmp[0] = '=';
+    }
+    i++;
+  }
+  return (ret && true);
+}
---
+++ mandatory/unset.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orahmoun <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 21:52:35 by orahmoun          #+#    #+#             */
+/*   Updated: 2022/03/11 22:26:29 by orahmoun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+t_env *remove_variable(t_env *env, char *variable)
+{
+  BEGIN
+  t_env *new_env;
+  t_env *tmp;
+
+  new_env = NULL;
+  while (env)
+  {
+    tmp = env->next;
+    env->next = NULL;
+    if (ft_strncmp(env->key, variable, ft_strlen(variable)) == 0
+          && (ft_strlen(variable) == ft_strlen(env->key)))
+    {
+      free(env->key);
+      free(env->value);
+      free(env);
+    }
+    else
+      add_variable_back(&new_env, env);
+    env = tmp;
+  }
+  END
+  return (new_env); 
+}
+
+int bunset(char **args, t_env **env)
+{
+  BEGIN
+  size_t i;
+
+  i = 0;
+  if (args == NULL)
+    return (0);
+  while (args[i])
+  {
+    *env = remove_variable(*env, args[i]);
+    i++;
+  }
+  END
+  return (0);
+}
